a55a1db75f9f041456a93f995e606eaf4cf2968c
gop <gameofpointers@proton.me>
2024-07-09 18:28:50 -0500

    Added the go routine inside the relay ph

--- core/slice.go
+++ core/slice.go
@@ -544,7 +544,7 @@ func (sl *Slice) relayPh(block *types.WorkObject, pendingHeaderWithTermini types
 	} else if !domOrigin && subReorg {
 		for _, i := range sl.randomRelayArray() {
 			if sl.subInterface[i] != nil {
-				sl.subInterface[i].SubRelayPendingHeader(pendingHeaderWithTermini, pendingHeaderWithTermini.WorkObject().ParentEntropy(nodeCtx), location, subReorg, nodeCtx)
+				go sl.subInterface[i].SubRelayPendingHeader(pendingHeaderWithTermini, pendingHeaderWithTermini.WorkObject().ParentEntropy(nodeCtx), location, subReorg, nodeCtx)
 			}
 		}
 	}
